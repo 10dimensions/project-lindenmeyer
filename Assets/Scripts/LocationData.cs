6b27ef725916bb155425be37df05a6beaec82009
10dimensions <38999489+10dimensions@users.noreply.github.com>
2019-05-08 17:23:01 +0530

    location from json

---
+++ Assets/Scripts/LocationData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class DataItem
+{
+    public string name;
+    public string type;
+    public List<string> location;
+}
+
+[System.Serializable]
+public class LocationData 
+{
+    public List<DataItem> LocationDataList;
+}
---
+++ Assets/Scripts/LocationDataHolder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationDataHolder : MonoBehaviour
+{   
+    //public LocationData LocationDataRoot;
+    public DataItem[] LocationDataRoot;
+
+    void Start()
+    {
+        //LocationDataRoot = new LocationData();
+        DeserializeData();
+    }
+
+    private void DeserializeData()
+    {
+        LocationDataRoot = JsonHelper.FromJson<DataItem>(LoadResourceTextfile("loc_data.json"));
+    }
+
+    public static string LoadResourceTextfile(string path)
+    {
+ 
+        string filePath = path.Replace(".json", "");
+        TextAsset targetFile = Resources.Load<TextAsset>(filePath);
+        print(targetFile.text);
+        return targetFile.text;
+    }
+}
