64fe025470553335a7e5b4a1fabc4e48f9eaeb89
10dimensions <38999489+10dimensions@users.noreply.github.com>
2019-05-09 18:49:00 +0530

    arutil  8th wall setup

--- Assets/Scripts/ARUtil.cs
+++ Assets/Scripts/ARUtil.cs
@@ -5,41 +5,95 @@ using UnityEngine.UI;
 
 public class ARUtil : MonoBehaviour
 {   
+    public static ARUtil Instance;
+
     public Button AddMeshButton;
     public Button ConfirmButton;
-    public Button SnapshotButton;
     public Button CancelButton;
+    public Button SnapshotButton;
     public Button BackButton;
 
-    private GameObject ToSpawnObj;
+    private GameObject SelectedObj;
+
+    private int maxMesh = 3;
+
+    void Awake()
+    {
+        if(Instance == null) Instance=this;
+    }
 
      void Start()
-    {
+    {   
+        MeshButtonPressed();
         AddMesh();
     }
 
     private void AddListenerToButtons()
     {
         AddMeshButton.onClick.AddListener(AddMesh);
+        ConfirmButton.onClick.AddListener(ConfimButtonPressed);
+
+        SnapshotButton.onClick.AddListener(TakeSnapShot);
+        BackButton.onClick.AddListener(BackButtonPressed);
     }
 
     private void AddMesh()
     {   
-        if(Singleton.Instance.isVegetation)
+        if(maxMesh>0)
         {
-            var go = Resources.Load<GameObject>("VCube");
-            
-            if (go == null) 
+            if(SingletonAR.Instance.isVegetation)
             {
-                Debug.Log("does not exist");
-                return;
+                var go = Resources.Load<GameObject>(SingletonAR.Instance.MeshName);
+                
+                if (go == null) 
+                {
+                    Debug.Log("does not exist");
+                    return;
+                }
+                
+                GameObject V = Instantiate(go) as GameObject;
+                V.transform.parent = GameObject.FindWithTag("arroot").transform;
+                V.SetActive(true);
+
+                maxMesh--;
+                SelectedObj = V;
             }
-            
-            GameObject V = Instantiate(go) as GameObject;
-            V.transform.parent = GameObject.FindWithTag("arroot").transform;
-            V.SetActive(true);
         }
+
+        MeshButtonPressed();
     }
+
+    private void MeshButtonPressed()
+    {
+        BackButton.interactable = false;
+        SnapshotButton.interactable = false;
+        AddMeshButton.interactable = false;
+
+        ConfirmButton.gameObject.SetActive(true);
+        CancelButton.gameObject.SetActive(true);
+    }
+
+    private void ConfimButtonPressed()
+    {
+        BackButton.interactable = true;
+        SnapshotButton.interactable = true;
+        AddMeshButton.interactable = true;
+
+        ConfirmButton.gameObject.SetActive(false);
+        CancelButton.gameObject.SetActive(false);
+    }
+
+    private void CancelButtonPressed()
+    {
+        BackButton.interactable = true;
+        SnapshotButton.interactable = true;
+        AddMeshButton.interactable = true;
+
+        ConfirmButton.gameObject.SetActive(false);
+        CancelButton.gameObject.SetActive(false);
+    }
+
+
 
     private void TakeSnapShot()
     {
---
+++ Assets/Scripts/SingletonAR.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonAR : MonoBehaviour
+{   
+    private static SingletonAR _instance;
+    public static SingletonAR Instance
+    {
+        get{ return _instance;}
+    }
+
+    public bool isVegetation = false;
+    public string MeshName;
+
+    void Awake()
+    {
+        if(_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+   
+}
